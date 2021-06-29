affc05f6248c344ce7d03b8284839ff614ebc748
chestarpro <chestarpro@gmail.com>
2021-06-29 22:12:51 +0600

    add Тask2

--- Task2/src/com/company/Main.java
+++ Task2/src/com/company/Main.java
@@ -8,6 +8,7 @@ public class Main {
         LinkedList<Person> persons = new LinkedList<>();
 
         addPerson(persons, new Person("Баланчаев", "Баланча", "Баланчаевич"));
+        addPerson(persons, new Person("Баланчаев", "Баланча", "Баланчаевич"));
         addPerson(persons, new Person("Иванов", "Иван", "Иванович"));
         addPerson(persons, new Person("Айтматов", "Чынгыз", "Торекулович"));
         addPerson(persons, new Person("Путын", "Владымыр", "Владымыровыч"));
@@ -22,9 +23,9 @@ public class Main {
                 if (value.equals(person)) throw new Exception("Человек №" + value.getId() +
                         " и " + "Человек №" + person.getId() + " идентичны!\n");
             }
+            persons.add(person);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        persons.add(person);
     }
 }
